2994a1a979dc192c337f09dd97e88fb483bef99a
BenAfonso <darkyler57@gmail.com>
2017-03-31 09:52:06 +0200

    doc<Comment and CommentFacade>: documentation on comment and commentFacade

--- DocBot-java/src/models/Comment.java
+++ DocBot-java/src/models/Comment.java
@@ -7,9 +7,7 @@ import java.util.Date;
  */
 public class Comment {
 
-    /**
-     *
-     */
+
     public int id;
     private String content;
     private String title;
@@ -31,59 +29,109 @@ public class Comment {
         this.setPatient_id(patient_id);
     }
 
-    /**
-     *
-     */
 
+    /**
+     * Get the content
+     * @return content
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Sets the content
+     *
+     * @param comment
+     */
     public void setContent(String comment) {
         this.content = comment;
     }
 
+    /**
+     * Get the title
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Set the title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Get the rating
+     * @return rate
+     */
     public int getRate() {
         return rate;
     }
 
+    /**
+     * Set the rating
+     * @param rating
+     */
     public void setRate(int rating) {
         this.rate = rating;
     }
 
-
+    /**
+     * get the post date
+     * @return date
+     */
     public Date getDatePost() {
         return datePost;
     }
 
+    /**
+     * set the date
+     * @param datepost
+     */
     public void setDatePost(Date datepost) {
         this.datePost = datepost;
     }
 
+    /**
+     * Returns the date
+     * @return dateString
+     */
     public String getDate() {
         return datePost.toString();
     }
 
+    /**
+     *
+     * Return the doctor_id
+     * @return doctor_id
+     */
     public int getDoctor_id() {
         return doctor_id;
     }
 
+    /**
+     * Set the doctor_id
+     * @param doctor_id
+     */
     public void setDoctor_id(int doctor_id) {
         this.doctor_id = doctor_id;
     }
 
+    /**
+     * Get the patient id
+     * @return patient_id
+     */
     public int getPatient_id() {
         return patient_id;
     }
 
+    /**
+     * Set the patient_id
+     * @param patient_id
+     */
     public void setPatient_id(int patient_id) {
         this.patient_id = patient_id;
     }
